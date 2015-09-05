f743da86772797fcbf30761239a644cdeaeec0c9
Brian Neaves <brian.g.neaves@gmail.com>
2015-09-05 13:48:56 -0400

    First commit

---
+++ js/clicker.js
@@ -0,0 +1,25 @@
+/*
+var count = 0;
+
+var elem = document.getElementById('catpic');
+elem.addEventListener('click', function(){
+	count = count + 1;
+  	console.log("I clicked!");
+}, false);
+*/
+
+/*
+console.log("i'm here!");
+$('h1').click(function(){
+	count = count + 1;
+	console.log(count);
+	console.log("I clicked!");
+	alert('You Clicked Me');
+});
+*/
+//console.log(count);
+
+/*$('.catpic').on("click","a,img", function (e) {
+      e.preventDefault();
+      alert('You Clicked Me');
+ });*/
